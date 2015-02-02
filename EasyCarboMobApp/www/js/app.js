69caa503921b9c5f0d23d4c98464a520b7e73982
Rafael S. Meneses <meneses.tio@gmail.com>
2015-02-02 17:53:42 -0300

    camera

--- EasyCarboMobApp/www/js/app.js
+++ EasyCarboMobApp/www/js/app.js
@@ -7,6 +7,7 @@
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
+
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -20,9 +21,9 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $compileProvider) {
   $stateProvider
-
+	$compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
   .state('app', {
     url: "/app",
     abstract: true,
